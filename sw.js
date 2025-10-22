329ecc203e4522cf6e30f1b3d52f63898d11e4b9
MrGreenJeans63 <bradhurley@icloud.com>
2025-10-22 12:46:16 -0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,19 @@
+// very small offline cache
+const CNAME = "medmeal-v1";
+const ASSETS = ["./","./index.html","./manifest.json"];
+
+self.addEventListener("install", e=>{
+  e.waitUntil(caches.open(CNAME).then(c=>c.addAll(ASSETS)));
+});
+self.addEventListener("activate", e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CNAME&&caches.delete(k)))));
+});
+self.addEventListener("fetch", e=>{
+  e.respondWith(
+    caches.match(e.request).then(r=> r || fetch(e.request).then(res=>{
+      const copy=res.clone();
+      caches.open(CNAME).then(c=>c.put(e.request, copy));
+      return res;
+    }).catch(()=>caches.match("./index.html")))
+  );
+});
